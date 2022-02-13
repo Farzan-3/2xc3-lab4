3e014f49c5431518afb846831851f408348fef8b
komaln226 <naeemk2@mcmaster.ca>
2022-02-12 21:41:14 -0500

    komal merge3

--- code.py
+++ code.py
@@ -43,6 +43,68 @@ def merge(left, right):
                 j+=1
     return L
 
+#mergesort_three
+def mergesort_three(L):
+    if len(L) <= 1:
+        return 
+    if len(L) == 2:
+        if L[0] > L[1]:
+            L[0], L[1] = L[1], L[0]
+        return 
+    mid = len(L)//3
+    left, right, middle = L[:mid], L[mid:mid*2], L[mid*2:]
+
+    mergesort_three(left)
+    mergesort_three(right)
+    mergesort_three(middle) 
+    temp = merge_three(left, right, middle)
+
+    for i in range(len(temp)):
+        L[i] = temp[i]
+
+#merge_three
+def merge_three(left, right, middle):
+    L = []
+    i = j = k = 0
+
+    while i < len(left) or j < len(right) or k < len(middle):
+        #Check it there's still elements to be merged from left and/or right
+        if i >= len(left):
+            if j >= len(right):
+                L.append(middle[k])
+                k +=1
+            elif k >= len(middle) or right[j] <= middle[k]:
+                L.append(right[j])
+                j += 1
+            else: 
+                L.append(middle[k])
+                k +=  1
+        elif j >= len(right):
+            if i >= len(left):
+                L.append(middle[k])
+            elif k >= len(middle) or left[i] <= right[j]:
+                L.append(left[i])
+                i += 1
+            else: 
+                L.append(middle[k])
+                k += 1
+        elif k >= len(middle):
+            if left[i] <= left[j]:
+                L.append(left[i])
+            else: 
+                L.append(right[j])
+                j += 1
+        else:
+            if left[i] <= right[j] and left[i] <= middle[k]:
+                L.append(left[i])
+                i += 1
+            elif right[j] <= left[i] and right[j] <= middle[k]:
+                L.append(right[j])
+                j += 1
+            else:
+                L.append(middle[k])
+                k+=1
+    return L     
 
 def create_random_list(n):
     L = []
